b9f29d65d7bd3c60b0257f7f5eb841bb319283b2
Mert Erdoğan <104645872+MertErdoganJr@users.noreply.github.com>
2023-04-17 22:57:35 +0300

    Add project files.

---
+++ Edukator.DataAccessLayer/Abstract/IContactInfoDal.cs
@@ -0,0 +1,15 @@
+﻿using Edukator.EntityLayer.Concreate;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Edukator.DataAccessLayer.Abstract
+{
+    public interface IContactInfoDal : IGenericDal<ContactInfo>
+    {
+
+    }
+}
+
