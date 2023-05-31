3ddda51c92203e80e9844138e0298744cb7bea08
Mert Erdoğan <104645872+MertErdoganJr@users.noreply.github.com>
2023-05-31 21:14:51 +0300

    last_part

---
+++ Edukator.PresentationLayer/Controllers/RoleAssignController.cs
@@ -0,0 +1,66 @@
+﻿using Edukator.EntityLayer.Concreate;
+using Edukator.PresentationLayer.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Edukator.PresentationLayer.Controllers
+{
+    public class RoleAssignController : Controller
+    {
+        private readonly RoleManager<AppRole> _roleManager;
+        private readonly UserManager<AppUser> _userManager;
+
+        public RoleAssignController(RoleManager<AppRole> roleManager, UserManager<AppUser> userManager)
+        {
+            _roleManager = roleManager;
+            _userManager = userManager;
+        }
+
+        public IActionResult Index()
+        {
+            var values = _userManager.Users.ToList();
+            return View(values);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> UserRoleAssign(int id)
+        {
+            var user = _userManager.Users.FirstOrDefault(x => x.Id == id);
+            TempData["userid"] = user.Id;
+            var roles = _roleManager.Roles.ToList();
+            var userRoles = await _userManager.GetRolesAsync(user);
+            List<RoleAssingViewModel> roleAssingViewModels = new List<RoleAssingViewModel>();
+            foreach (var item in roles)
+            {
+                RoleAssingViewModel model = new RoleAssingViewModel();
+                model.RoleID = item.Id;
+                model.RoleName = item.Name;
+                model.RoleExist = userRoles.Contains(item.Name);
+                roleAssingViewModels.Add(model);
+            }
+            return View(roleAssingViewModels);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> UserRoleAssign(List<RoleAssingViewModel> model)
+        {
+            var userid = (int)TempData["userid"];
+            var user = _userManager.Users.FirstOrDefault(x=>x.Id == userid);
+            foreach (var item in model)
+            {
+                if (item.RoleExist)
+                {
+                    await _userManager.AddToRoleAsync(user, item.RoleName);
+                }
+                else
+                {
+                    await _userManager.RemoveFromRoleAsync(user, item.RoleName);
+                }
+            }
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ Edukator.PresentationLayer/Models/RoleAssingViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace Edukator.PresentationLayer.Models
+{
+    public class RoleAssingViewModel
+    {
+        public int RoleID { get; set; }
+        public string RoleName { get; set; }
+        public bool RoleExist { get; set; }
+
+    }
+}
